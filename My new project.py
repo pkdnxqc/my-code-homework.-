f97f200e28e3570034ab13b1a2239ac0189ab8d6
Darrshan <pkdnxqc@gmail.com>
2026-05-04 17:46:35 +0530

    Add files via upload

---
+++ My new project.py
@@ -0,0 +1,10 @@
+print("i know about world war1&2")
+yer =4
+print("i reserched about it  for ",yer,"years; ")
+print("My hobbies are learning about calculus")
+print("My favorite food? My favorite food is mango juice. ")
+print("My favorite place is kochin")
+# this my list of things faver the most
+years = 5
+
+print (",years,"years;)
