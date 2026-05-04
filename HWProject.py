1a7020aa16afcb6734e5a962df38a15c3d452241
Darrshan <pkdnxqc@gmail.com>
2026-05-04 17:48:20 +0530

    Update HWProject.py

---
+++ HWProject.py
@@ -0,0 +1,17 @@
+print ("My Daily Routine ")
+#To go to my school. 
+print("I wake up at 6:00. ")
+#I became hygienic. 
+print("By brushing my teeth and bathing ")
+#I become strong I'm keeping my body fit. 
+print("Do push-up and pull-ups and by training ")
+#I eat a healthy meal like 
+print("Rice and eggs And if needed I eat dal. ")
+#In school, I will 
+print("I will have P.T., and I will learn new subjects, new things. ")
+#In school I will also play. 
+print("They are written as just for my extracurricular sports inside of school after school ended. ")
+#After coming home, I will 
+print("After coming home, I will watch a little bit of TV, eat salads, and I will drink a little bit of milk to make me go to tuition. ")
+#After going to tuition, I will 
+print("Eat my dinner, and I will brush my teeth, and I will sleep. ")
